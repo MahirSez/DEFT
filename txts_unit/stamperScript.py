c8629d65c1b1bd80698c84f9c4f49cda5f97fc34
MahirSez <mahirsezan@gmail.com>
2022-04-28 22:29:14 +0600

    packet forwarding works

--- txts_unit/stamperScript.py
+++ txts_unit/stamperScript.py
@@ -1,14 +1,16 @@
 from scapy.all import *
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, ICMP
+from scapy.layers.l2 import Ether
 
 
 def forward_to_sw2(pkt):
     print("=========================")
     print(pkt.summary())
     pkt[IP].src = '10.0.0.4'
-    del pkt[IP].chksum
+    pkt[Ether].src = '00:00:00:00:00:04'
     sendp(pkt, iface='stamper-eth0')
 
 
 print("Stamper sniffing packets on stamper-eth0....")
 sniff(filter='ip src host 10.0.0.1', iface="stamper-eth0", prn=forward_to_sw2)
+
