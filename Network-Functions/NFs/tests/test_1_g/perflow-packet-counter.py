f2705b063e08613ac6a970009823a190ae3ad240
ComeBackCity <gourabsaha567@gmail.com>
2022-04-29 00:45:40 +0600

    Experiment 1(g) added

--- Network-Functions/NFs/tests/test_1_g/perflow-packet-counter.py
+++ Network-Functions/NFs/tests/test_1_g/perflow-packet-counter.py
@@ -19,7 +19,6 @@ from exp_package.Two_phase_commit.primary_2pc import Primary
 per_flow_packet_counter = None
 master = None
 
-
 class Buffers():
     input_buffer = queue.Queue(maxsize=100000)          # (pkts, pkts_id) tuples
     output_buffer = queue.Queue(maxsize=100000)         # (pkts, pkts_id) tuples
@@ -36,9 +35,9 @@ class Timestamps():
 
 
 class Limit():
-    BATCH_SIZE = 20
+    BATCH_SIZE = 10
     PKTS_NEED_TO_PROCESS = 1000
-    GLOBAL_UPDATE_FREQUENCY = 15
+    BUFFER_LIMIT = 2 * BATCH_SIZE
 
 
 class Statistics():
@@ -46,14 +45,10 @@ class Statistics():
     received_packets = 0
     total_delay_time = 0
     total_three_pc_time = 0
+    packet_dropped = 0
 
 # (st_time, en_time) - processing time
 # additional 3pc time 
-
-# glob_freq = 2, batch = 5
-# [1, 2, 3, 4, 5, 6]
-# [0, 1, 0, 1, 0, 1] -> glob 
-# 
 
 # input_buffer
 # output_buffer
@@ -70,12 +65,14 @@ def receive_a_pkt(pkt):
     Statistics.received_packets += 1
     # redis_client.incr("packet_count " + host_var)
     
-    Buffers.input_buffer.put((pkt, Statistics.received_packets))
-    BufferTimeMaps.input_in[Statistics.received_packets] = Helpers.get_current_time_in_ms()
+    if Buffers.input_buffer.qsize() < Limit.BUFFER_LIMIT:
+        Buffers.input_buffer.put((pkt, Statistics.received_packets))
+        BufferTimeMaps.input_in[Statistics.received_packets] = Helpers.get_current_time_in_ms()
+    else:
+        Statistics.packet_dropped += 1
 
 
 def process_a_packet(packet, packet_id):
-
     Statistics.processed_pkts += 1
     Statistics.total_delay_time += Helpers.get_current_time_in_ms() - BufferTimeMaps.input_in[packet_id]
 
@@ -85,34 +82,23 @@ def process_a_packet(packet, packet_id):
     Buffers.output_buffer.put((packet, packet_id))
     BufferTimeMaps.output_in[packet_id] = Helpers.get_current_time_in_ms()
 
-
 def process_packet_with_hazelcast():
 
-    pkt_num_of_cur_batch = 0
-    uniform_global_distance = Limit.BATCH_SIZE // Limit.GLOBAL_UPDATE_FREQUENCY
-    
     while True:
-
         pkt, pkt_id = Buffers.input_buffer.get()
 
         process_a_packet(pkt, pkt_id)
 
-        pkt_num_of_cur_batch += 1
-
-
         if Buffers.output_buffer.qsize() == Limit.BATCH_SIZE:
-            pkt_num_of_cur_batch = 0
             empty_output_buffer()
-            local_state_update()
-
-        if pkt_num_of_cur_batch  % uniform_global_distance == 0 or pkt_num_of_cur_batch == Limit.BATCH_SIZE: 
-            global_state_update(10)
+            per_batch_update()
 
         if Statistics.processed_pkts == Limit.PKTS_NEED_TO_PROCESS:
             # time_delta = Helpers.get_current_time_in_ms() - Timestamps.start_time
             # process_time = time_delta / 1000.0 + Statistics.total_three_pc_time
 
-            print(f'Latency for batch-size {Limit.BATCH_SIZE} is {Statistics.total_delay_time / Statistics.processed_pkts} ms/pkt')
+            print(f'Latency for batch-size {Limit.BATCH_SIZE} is {Statistics.total_delay_time/ Statistics.processed_pkts} ms/pkt')
+            print(f'packets dropped {Statistics.packet_dropped}')
             break
 
 
@@ -123,20 +109,20 @@ def empty_output_buffer():
         # print(f'current pkt {pkt_id}')
         Statistics.total_delay_time += Helpers.get_current_time_in_ms() - BufferTimeMaps.output_in[pkt_id]
 
-def local_state_update():
-    # local state update    
+
+
+def per_batch_update():
+    map_key = "global"
     print("------------------------------------------------------------------------------------------------------")
     print(f'replicating on backup as per batch.\n cur_batch: {State.per_flow_cnt}')
     cur_time = Helpers.get_current_time_in_ms()
     master.replicate(State.per_flow_cnt)
     Statistics.total_three_pc_time += Helpers.get_current_time_in_ms() - cur_time
 
-def global_state_update(batches_processed: int):
-    # global state update
-    map_key = "global"
+
     per_flow_packet_counter.lock(map_key)
     value = per_flow_packet_counter.get(map_key)
-    per_flow_packet_counter.set(map_key, batches_processed if value is None else value + batches_processed)
+    per_flow_packet_counter.set(map_key, 1 if value is None else value + 1)
     per_flow_packet_counter.unlock(map_key)
 
 # def set_host_var():
