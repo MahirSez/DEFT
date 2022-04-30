157aed3e7d6a9b7e17778003495233ce050ab22d
MahirSez <mahirsezan@gmail.com>
2022-05-01 00:10:17 +0600

    added ../pox/ft_net/noStamperController.py

---
+++ pox/ft_net/noStamperController.py
@@ -0,0 +1,176 @@
+# Copyright 2012 James McCauley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This component is for use with the OpenFlow tutorial.
+
+It acts as a simple hub, but can be modified to act like an L2
+learning switch.
+
+It's roughly similar to the one Brandon Heller did for NOX.
+
+./pox.py log.level --DEBUG misc.of_tutorial misc.full_payload
+
+Iperf: flow_out -> *** Results: ['582 Kbits/sec', '1.54 Mbits/sec']
+
+
+
+"""
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet.ethernet import *
+import config
+
+log = core.getLogger()
+
+switchID = 1
+
+
+macToName = config.MAC_TO_HOST
+macToName['ff:ff:ff:ff:ff:ff'] = 'flood'
+
+
+CLIENT_MAC = EthAddr(config.HOST_MAC['client'])
+
+
+class Tutorial(object):
+	"""
+	A Tutorial object is created for each switch that connects.
+	A Connection object for that switch is passed to the __init__ function.
+	"""
+
+	# init is called for each switch
+	def __init__(self, connection):
+		global switchID
+		# Keep track of the connection to the switch so that we can
+		# send it messages!
+		self.connection = connection
+		# This binds our PacketIn event listener
+		connection.addListeners(self)
+
+		self.switchID = switchID
+		self.switchName = 'switch-' + str(self.switchID)
+		switchID += 1
+
+		# Use this table to keep track of which ethernet address is on
+		# which switch port (keys are MACs, values are ports).
+
+		self.mac_to_port = {}
+
+		log.debug("Switch ID = " + str(self.switchID))
+
+
+
+
+	def resend_packet(self, packet_in, out_port):
+		"""
+		Instructs the switch to resend a packet that it had sent to us.
+		"packet_in" is the ofp_packet_in object the switch had sent to the
+		controller due to a table-miss.
+		"""
+		msg = of.ofp_packet_out()
+		msg.data = packet_in
+
+		# Add an action to send to the specified port
+		action = of.ofp_action_output(port=out_port)
+		msg.actions.append(action)
+
+		# Send message to switch
+		self.connection.send(msg)
+
+	def act_like_hub(self, packet, packet_in):
+		"""
+		Implement hub-like behavior -- send all packets to all ports besides
+		the input port.
+		"""
+
+		# We want to output to all ports -- we do that using the special
+		# OFPP_ALL port as the output port.  (We could have also used
+		# OFPP_FLOOD.)
+		self.resend_packet(packet_in, of.OFPP_FLOOD)
+
+	def act_like_switch(self, packet, packet_in):
+		"""
+		Implement switch-like behavior.
+		"""
+
+		# Learn the port for the source MAC
+		self.mac_to_port[packet.src] = packet_in.in_port
+
+
+		# srcName = "unknown" if str(packet.src) not in macToName else macToName[str(packet.src)]
+		# dstName = "unknown" if str(packet.dst) not in macToName else macToName[str(packet.dst)]
+		# packetType = ethtype_to_str(packet.type)
+		# print(self.switchName + " : " + srcName + " --> " + dstName)
+		# print(self.switchName + " : " + str(packet.src) + " --> " + str(packet.dst) + " :: " + packetType)
+
+		# print(self.mac_to_port)
+
+
+		# if the port associated with the destination MAC of the packet is known:
+		if packet.dst in self.mac_to_port:
+
+			
+			out_port = self.mac_to_port[packet.dst]
+
+			msg = of.ofp_flow_mod()
+
+			# Set fields to match received packet
+			msg.match = of.ofp_match.from_packet(packet)
+			msg.match.in_port = packet_in.in_port
+
+			# print("Packets from " + str(packet.src) + " to " + str(packet.dst) + " will go through port " + str(out_port))
+			msg.actions.append(of.ofp_action_output(port=out_port))
+
+			dst_name = macToName[str(packet.dst)] if str(packet.dst) in macToName else None
+
+			if self.switchID == 2 and packet.src == CLIENT_MAC and packet.type != packet.ARP_TYPE and dst_name in config.PRIMARY_TO_SECONDARY:
+				secondary_name = config.PRIMARY_TO_SECONDARY[dst_name]
+				secondary_port = config.SWITCH_HOST_TO_PORT[self.switchName][secondary_name]
+				msg.actions.append(of.ofp_action_output(port=secondary_port))
+
+			msg.data = packet_in
+			self.connection.send(msg)
+		else:
+			# Flood the packet out everything but the input port
+			self.resend_packet(packet_in, of.OFPP_ALL)
+
+	def _handle_PacketIn(self, event):
+		"""
+		Handles packet in messages from the switch.
+		"""
+
+		packet = event.parsed  # This is the parsed packet data.
+		if not packet.parsed:
+			log.warning("Ignoring incomplete packet")
+			return
+
+		packet_in = event.ofp  # The actual ofp_packet_in message.
+
+		# Comment out the following line and uncomment the one after
+		# when starting the exercise.
+		# self.act_like_hub(packet, packet_in)
+		self.act_like_switch(packet, packet_in)
+
+
+def launch():
+	"""
+	Starts the component
+	"""
+	def start_switch(event):
+		log.debug("Controlling %s" % (event.connection,))
+		Tutorial(event.connection)
+
+	core.openflow.addListenerByName("ConnectionUp", start_switch)
