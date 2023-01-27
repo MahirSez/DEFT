a976dc51a9701e338c140a46b5b0981f13cb0666
MahirSez <mahirsezan@gmail.com>
2023-01-27 10:58:16 +0000

    packet now duplicates to secondary from the switch

--- src/txts_unit/net_setup.sh
+++ src/txts_unit/net_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo ovs-vsctl del-br ovs-br1
+sudo ovs-vsctl --if-exists del-br ovs-br1
 sudo ovs-vsctl add-br ovs-br1
 
+# reads configuration from .env
 HZ_CLIENT_CNT=$(grep HZ_CLIENT_CNT .env | cut -d '=' -f2)
 HZ_CLIENT_IP_PATTERN=$(grep HZ_CLIENT_IP_PATTERN .env | cut -d '=' -f2)
 
@@ -9,26 +10,46 @@ HZ_CLIENT_IP_PATTERN=$(grep HZ_CLIENT_IP_PATTERN .env | cut -d '=' -f2)
 # sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up
 sudo ifconfig ovs-br1 "${HZ_CLIENT_IP_PATTERN/$/1}" netmask 255.255.255.0 up
 
+
+# adds stamper module with last octet = 230
+stamper_ip=${HZ_CLIENT_IP_PATTERN/$/230}/24
+command="sudo ovs-docker add-port ovs-br1 eth1 txts_unit_stamper_1 --ipaddress=$stamper_ip"
+echo "$command"
+$command
+
+
 for ((i=1;i<=HZ_CLIENT_CNT;i++)); 
 do
-    client_ip=${HZ_CLIENT_IP_PATTERN/$/$((i + 1))}/24
+    # adds primary nf to the switch
+    primary_ip=${HZ_CLIENT_IP_PATTERN/$/$((i + 1))}
+    primary_ip_with_mask=$primary_ip/24
     client_name=txts_unit_hz_client_$i
-    command="sudo ovs-docker add-port ovs-br1 eth1 $client_name --ipaddress=$client_ip"
+    primary_nf_command="sudo ovs-docker add-port ovs-br1 eth1 $client_name --ipaddress=$primary_ip_with_mask" 
     
-    echo "$command"
-    $command
-done
+    echo "$primary_nf_command"
+    $primary_nf_command
 
 
-for ((i=1;i<=HZ_CLIENT_CNT;i++)); 
-do
-    secondary_ip=${HZ_CLIENT_IP_PATTERN/$/$((i + 1 + 100))}/24
+    # adds corresponding secondary nf to the switch
+    secondary_ip=${HZ_CLIENT_IP_PATTERN/$/$((i + 1 + 100))}
+    seondary_ip_with_mask=$secondary_ip/24
     secondary_name=txts_unit_secondary_$i
-    command="sudo ovs-docker add-port ovs-br1 eth1 $secondary_name --ipaddress=$secondary_ip"
+    secondary_nf_command="sudo ovs-docker add-port ovs-br1 eth1 $secondary_name --ipaddress=$seondary_ip_with_mask"
     
-    echo "$command"
-    $command
+    echo "$secondary_nf_command"
+    $secondary_nf_command
+
+
+    # adds flow rule to duplicate packet to secondary
+    packet_dup_command="sudo ovs-ofctl add-flow ovs-br1 dl_type=0x0800,nw_proto=17,nw_dst=$primary_ip,actions=normal,mod_nw_dst:$secondary_ip,normal"
+    echo "$packet_dup_command"
+    $packet_dup_command
+
 done
 
-# sudo ovs-docker add-port ovs-br1 eth1 txts_unit_hz_client_1 --ipaddress=173.16.1.2/24
-# sudo ovs-docker add-port ovs-br1 eth1 txts_unit_hz_client_2 --ipaddress=173.16.1.3/24
+
+
+# primary -> sudo ovs-docker add-port ovs-br1 eth1 txts_unit_hz_client_1 --ipaddress=173.16.1.2/24 
+# secondary -> sudo ovs-docker add-port ovs-br1 eth1 txts_unit_hz_client_2 --ipaddress=173.16.1.102/24
+# stamper -> sudo ovs-docker add-port ovs-br1 eth1 txts_unit_stamper_1 --ipaddress=173.16.1.230/24
+# flow-rule -> sudo ovs-ofctl add-flow ovs-br1 in_port=3,dl_type=0x0800,nw_proto=17,actions=normal,mod_nw_dst:173.16.1.3,normal
